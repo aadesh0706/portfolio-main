5ea32ca81125063b38f92f308c5646446dcdd586
Aadesh Gulumbe <71577243+aadesh0706@users.noreply.github.com>
2024-10-06 15:44:30 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,10 +12,19 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+// export const metadata = {
+//   title: "Aadesh Gulumbe",
+//   description: "I'm a passionate software developer currently pursuing a degree in Information Technology at PCET's NMIET Talegaon Dabhade, Pune. With over 35 repositories and numerous projects across different fields like web development, IoT, and mobile apps, I thrive on solving real-world problems using innovative tech solutions.",
+// };
 export const metadata = {
-  title: "Aadesh Gulumbe",
+  title: "Aadesh Gulumbe | Portfolio",
   description: "I'm a passionate software developer currently pursuing a degree in Information Technology at PCET's NMIET Talegaon Dabhade, Pune. With over 35 repositories and numerous projects across different fields like web development, IoT, and mobile apps, I thrive on solving real-world problems using innovative tech solutions.",
+  keywords: "Aadesh Gulumbe, software developer, portfolio, web development, IoT, mobile apps, IT student, coding, programming, GitHub projects",
+  author: "Aadesh Gulumbe",
+  viewport: "width=device-width, initial-scale=1",
+  robots: "index, follow", // Allow search engines to index and follow links
 };
+
 
 export default function RootLayout({ children }) {
   return (
